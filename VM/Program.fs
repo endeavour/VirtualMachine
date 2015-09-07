eeff29db938b4e0b00ae286c1b684793ee99a90c
James Freiwirth <james@perfectshuffle.co.uk>
2015-09-07 01:45:07 +0100

    Switch out .NET BCL Stack for an immutable linked list

--- VM/Program.fs
+++ VM/Program.fs
@@ -7,9 +7,8 @@
 type MachineState =
  {
    IP : int
-   Stack : System.Collections.Generic.Stack<byte>
+   Stack : byte list
  }
-  
 
 [<EntryPoint>]
 let main argv =
@@ -21,37 +20,43 @@ let main argv =
         match (op |> enum) with
         | OpCode.Halt -> printfn "HALTED"
         | OpCode.IntPrint ->
-          let value = [|state.Stack.Pop(); state.Stack.Pop(); state.Stack.Pop(); state.Stack.Pop()|]          
-          printfn "%d" (System.BitConverter.ToInt32(value, 0))
-          cpu instructions {state with IP = state.IP + 1}
+          match state.Stack with
+          | a::b::c::d::xs ->
+            printfn "%d" (System.BitConverter.ToInt32([|a;b;c;d|], 0))
+            cpu instructions {state with IP = state.IP + 1; Stack = xs}
+          | _ -> printfn "No int on stack. Aborting."
         | OpCode.IntPush ->
           let value = instructions.[ip+1]
           let bytes = System.BitConverter.GetBytes(value) |> Array.rev
-          bytes |> Seq.iter state.Stack.Push
-          cpu instructions {state with IP = state.IP + 2}
+          cpu instructions {state with IP = state.IP + 2; Stack = bytes.[3]::bytes.[2]::bytes.[1]::bytes.[0]::state.Stack}
         | OpCode.Add ->
-          let value1 = System.BitConverter.ToInt32([|state.Stack.Pop(); state.Stack.Pop(); state.Stack.Pop(); state.Stack.Pop()|],0)
-          let value2 = System.BitConverter.ToInt32([|state.Stack.Pop(); state.Stack.Pop(); state.Stack.Pop(); state.Stack.Pop()|],0)
+          match state.Stack with
+          | a::b::c::d::e::f::g::h::xs ->
+          let value1 = System.BitConverter.ToInt32([|a;b;c;d|],0)
+          let value2 = System.BitConverter.ToInt32([|e;f;g;h|],0)
           let sum = value1 + value2
           let bytes = System.BitConverter.GetBytes(sum) |> Array.rev
-          bytes |> Seq.iter state.Stack.Push
-          cpu instructions {state with IP = state.IP + 1}
+          cpu instructions {state with IP = state.IP + 1; Stack = bytes.[3]::bytes.[2]::bytes.[1]::bytes.[0]::xs}
+          | _ -> printfn "Two ints not found on stack. Aborting."
         | _ -> invalidOp "Invalid opcode"
       | _ -> printf "DONE"
 
     let program =
       [|
         int OpCode.IntPush
-        5
+        10
         int OpCode.IntPush
-        7
+        90
+        int OpCode.IntPush
+        23
+        int OpCode.Add
         int OpCode.Add
         int OpCode.IntPrint
       |]
 
     printfn "Running..."
 
-    cpu program {IP = 0; Stack = System.Collections.Generic.Stack<byte>()}
+    cpu program {IP = 0; Stack = []}
 
     System.Console.ReadKey() |> ignore
     0 // return an integer exit code
